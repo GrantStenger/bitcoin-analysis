95f024ec457a476c0329f69ba5038a2a2e298cd9
Grant Stenger <ghstenge@usc.edu>
2017-12-26 00:45:27 -0800

    Updating analysis

--- bitcoinAPI.py
+++ bitcoinAPI.py
@@ -9,6 +9,11 @@ upCount = 0
 downCount = 0
 trendingCount = 0
 reversalCount = 0
+percentageMoveAfterUp = []
+percentageMoveAfterDown = []
+testPercentSum = 0
+testPercentProduct = 1
+upProduct = 1
 
 # As compared with Day 1's price, decide is Day 2 is up or down
 if df.loc[1, 'Close Price'] > df.loc[0, 'Close Price']:
@@ -25,6 +30,21 @@ for i in range(df['Close Price'].count() - 1):
 	else:
 		today = "Down"
 		downCount += 1
+
+	# Fill arrays holding the percentage increase or decrease if yesterday was up or down
+	if previousDay == "Up":
+		percentChange = (df.loc[i+1, 'Close Price'] / df.loc[i, 'Close Price'])-1
+		print("The price when up " + '{:.2%}'.format(percentChange) + " today.")
+		print("You were long.")
+		percentageMoveAfterUp.append(percentChange)
+		upProduct = upProduct * (percentChange+1)
+	else:
+		percentChange = (df.loc[i+1, 'Close Price'] / df.loc[i, 'Close Price'])-1
+		print("The price when up " + '{:.2%}'.format(percentChange) + " today.")
+		print("You were short.")
+		percentageMoveAfterDown.append(percentChange)
+	testPercentSum = testPercentSum + (df.loc[i+1, 'Close Price'] / df.loc[i, 'Close Price'])-1
+	testPercentProduct = testPercentProduct*(df.loc[i+1, 'Close Price'] / df.loc[i, 'Close Price'])
 
 	# Compare today with yesterday, if both are up or both are down, the stock is trending
 	if today == previousDay:
@@ -65,3 +85,33 @@ total = trendingCount + reversalCount
 print("Trending percentage: " + str('{:.1%}'.format(trendingCount/total)))
 print("Reversal percentage: " + str('{:.1%}'.format(reversalCount/total)))
 print()
+
+upSum = 0
+downSum = 0
+print("You were long for these: ")
+for item in percentageMoveAfterUp:
+	print('{:.2%}'.format(item))
+	upSum = upSum + item
+
+print()
+print()
+print()
+
+print("You were short for these: ")
+for item in percentageMoveAfterDown:
+	print('{:.2%}'.format(item))
+	downSum = downSum + item
+
+print()
+print()
+
+print("Average daily return when long: " + '{:.2%}'.format(upSum/upCount))
+print("Average daily return when short: " + '{:.2%}'.format(downSum/downCount))
+print("testPercentProduct: " + '{:.2%}'.format(testPercentProduct))
+print("Actual return: " + '{:.2%}'.format(df.loc[df['Close Price'].count() - 1, 'Close Price']/df.loc[0, 'Close Price']))
+print("Return from just investing when up: " + '{:.2%}'.format(upProduct))
+print()
+if testPercentProduct > upProduct:
+	print("Not a great strategy, would have made more just holding all year.")
+else:
+	print("Good. This strategy is better than simply holding all year.")
